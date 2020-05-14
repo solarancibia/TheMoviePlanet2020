96926b247eeaa7f223d586b5bb1a0efff87ff6b5
Sol <srodriguezarancibia@udesa.edu.ar>
2020-05-14 16:11:37 -0300

    Agregue rutas

--- routes/movies.js
+++ routes/movies.js
@@ -8,6 +8,8 @@ const controller = require('../controllers/movieController');
 router.get('/', controller.home); 
 router.get('/Home', controller.home); 
 router.get('/ListadoDeGeneros', controller.listadoDeGeneros); 
-
-
+router.get('/PeliculasPorGenero', controller.peliculasPorGenero); 
+router.get('/DetallePeli', controller.detallePeli); 
+router.get('/PelisPreferidas', controller.pelisPreferidas); 
+router.get('/Search', controller.resultadoDelBuscador); 
 module.exports = router;
