2331aa31a75ba9f3e472334e185e0386eabca406
Sol <srodriguezarancibia@udesa.edu.ar>
2020-05-13 23:23:39 -0300

    agregue un par de rutas

--- routes/movies.js
+++ routes/movies.js
@@ -6,5 +6,8 @@ const controller = require('../controllers/movieController');
 
 //Ruta de prueba
 router.get('/', controller.home); 
+router.get('/Home', controller.home); 
+router.get('/ListadoDeGeneros', controller.listadoDeGeneros); 
+
 
 module.exports = router;
