2af3eba47f226a1b171eb3bce6c6f91d5803a8d9
Sol <srodriguezarancibia@udesa.edu.ar>
2020-05-27 19:02:01 -0300

    boludez

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,5 +2,8 @@ const db = require('../database/models');
 const modulo = require('../modulo-login');
 
 module.exports = {
-
+// login: function(req,res){
+//     let username = req.body.name;
+//     let password = req.body.password;
+// }
 }
