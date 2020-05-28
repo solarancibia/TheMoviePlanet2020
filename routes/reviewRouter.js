76726af818700914c1db2a9af5e0e6d6f10e2fde
ema <ejuarez@udesa.edu.ar>
2020-05-28 15:09:36 -0300

    ruta review

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -4,5 +4,7 @@ var router = express.Router();
 //Controller
 const controller = require('../controllers/reviewController');
 
+router.get("/", controller.index);
+router.post("/", controller.review );
 
 module.exports = router;
