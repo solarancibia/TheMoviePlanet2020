8d3c404d3a1530b0018238edb05d5cf959ba8d5b
Sol <srodriguezarancibia@udesa.edu.ar>
2020-05-28 15:22:14 -0300

    review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,25 @@
 const db = require('../database/models');
-const bcrypt = require('bcryptjs');
+const op = db.Sequelize.Op;
+const moduloLogin = require ('../login.js')
 
 module.exports = {
-    
-};
+    index: function (req, res){
+
+    },
+
+    //Crear review
+    review: (req, res) =>{
+        moduloLogin.validate(req.body.email, req.body.password)
+    .then (result => {
+        db.Reviews.create({
+            text: req.body.text,
+            score: req.body.score,
+            user_id: result.id,
+            movie_id: req.params.movieID
+        })
+        .then(function(index)){
+            return res.redirect('/movies?id=' + req.params.movieID)
+        }
+    })
+    }
+ };
