f82e17a54b357f358b860968492e708de6e25122
Sol <srodriguezarancibia@udesa.edu.ar>
2020-06-03 18:19:57 -0300

    algo

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -4,21 +4,8 @@ var router = express.Router();
 //Controller
 const controller = require('../controllers/loginController');
 
-// router.get("/", controller.login);
-// router.get("/", controller.entrar);
+router.get("/", controller.login);
+router.get("/", controller.entrar);
 
-router.get ('/', controller.logUser);
-
-router.post ('/login', controller.confirmUser);
-
-router.get ('/reviews/:id', controller.getReviews);
-
-router.get ('/reviews/edit/:id', controller.showEdit);
-
-router.post ('/reviews/edit/:id', controller.confirmEdit);
-
-router.get ('/reviews/delete/:id', controller.deleteReview);
-
-router.post ('/reviews/delete/:id', controller.confirmDelete);
 
 module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,5 +7,19 @@ const controller = require('../controllers/usersController');
 router.get("/", controller.register);
 router.post("/", controller.save );
 
+router.get ('/', controller.logUser);
+
+router.post ('/login', controller.confirmUser);
+
+router.get ('/reviews/:id', controller.getReviews);
+
+router.get ('/reviews/edit/:id', controller.showEdit);
+
+router.post ('/reviews/edit/:id', controller.confirmEdit);
+
+router.get ('/reviews/delete/:id', controller.deleteReview);
+
+router.post ('/reviews/delete/:id', controller.confirmDelete);
+
 
 module.exports = router;
