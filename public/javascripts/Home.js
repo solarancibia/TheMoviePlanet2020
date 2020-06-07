4525b98a7ddc2440691f4d1ef13bb8922a821f9d
Sol <srodriguezarancibia@udesa.edu.ar>
2020-06-07 12:20:28 -0300

    Saque los botones de favoritos

--- public/javascripts/Home.js
+++ public/javascripts/Home.js
@@ -20,7 +20,7 @@ fetch("https://api.themoviedb.org/3/trending/all/day?api_key=d72b8119ca0d802447e
 
       var li;
       li = '<li>'
-      li += "<button onclick='agregarFavoritos("+id+")' class='estrellita'> &#9733; </button>"
+      // li += "<button onclick='agregarFavoritos("+id+")' class='estrellita'> &#9733; </button>"
       li += "<a href='/movies/detallePeli?idDePeli="+id+"'>"
       li += '<img src=' + url + img + '>'
       li += '<div class="uk-position-center uk-panel divPelis"><h1 class="headersHome">' + titulo + '</h1></div>'
@@ -58,7 +58,7 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
 
       var li;
       li = '<li>'
-      li += "<button onclick='agregarFavoritos("+id+")' class='estrellita'> &#9733; </button>"
+      // li += "<button onclick='agregarFavoritos("+id+")' class='estrellita'> &#9733; </button>"
       li += "<a href='/movies/detallePeli?idDePeli="+id+"'>"
       li += '<img src=' + url + img + '>'
       li += '<div class="uk-position-center uk-panel divPelis"><h1 class="headersHome">' + titulo + '</h1></div>'
@@ -93,7 +93,7 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
 
        var li;
        li = '<li>'
-       li += "<button onclick='agregarFavoritos("+id+")' class='estrellita'> &#9733; </button>"
+      //  li += "<button onclick='agregarFavoritos("+id+")' class='estrellita'> &#9733; </button>"
        li += "<a href='/movies/detallePeli?idDePeli="+id+"'>"
        li += '<img src=' + url + img + '>'
        li += '<div class="uk-position-center uk-panel divPelis"><h1 class= "headersHome">' + titulo + '</h1></div>'
