836ce9c8766f348c0891d6b7c371dd0b27186052
ema <ejuarez@udesa.edu.ar>
2020-06-29 12:40:11 -0300

    comentarios

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //Controller
 const controller = require('../controllers/reviewController');
 
-
+//las resenas en el detalle de las peliculas 
 router.get("/", controller.index);
 router.post("/", controller.review);
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,18 +7,19 @@ const controller = require('../controllers/usersController');
 router.get("/", controller.register);
 router.post("/", controller.save );
 
+//ruta del login
 router.get ('/login', controller.logUser);
-
 router.post ('/login', controller.confirmUser);
 
+//ruta de mis reviews
 router.get ('/reviews/:id', controller.getReviews);
 
+//ruta donde edito mi resena 
 router.get ('/reviews/edit/:id', controller.showEdit);
-
 router.post ('/reviews/edit/:id', controller.confirmEdit);
 
+//ruta done elimino mi resena
 router.get ('/reviews/delete/:id', controller.deleteReview);
-
 router.post ('/reviews/delete/:id', controller.confirmDelete);
 
 
